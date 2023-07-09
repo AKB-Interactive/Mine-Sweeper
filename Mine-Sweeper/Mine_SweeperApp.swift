3bf1266576e611699305f0ff18fdd84e903fd9bb
aaronalpern <aaron.alpern@outllook.com>
2023-07-09 12:24:36 -0700

    Initial Commit

---
+++ Mine-Sweeper/Mine_SweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mine_SweeperApp.swift
+//  Mine-Sweeper
+//
+//  Created by Aaron Alpern on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Mine_SweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
